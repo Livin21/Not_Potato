32c92a7b774175ae98f5e0246172eb63cda20895
livin <livinmathew99@gmail.com>
2017-05-30 18:05:35 +0530

    finished prediction

---
+++ potato_not_potato.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+import sys
+
+# change this as you see fit
+image_path = sys.argv[1]
+
+# Read in the image_data
+image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+import os
+import shutil
+from os import listdir
+from os import mkdir
+from shutil import copyfile
+from os.path import isfile, join
+
+
+# Loads label file, strips off carriage return
+label_lines = [line.rstrip() for line 
+                   in tf.gfile.GFile("./retrained_labels.txt")]
+
+# Unpersists graph from file
+with tf.gfile.FastGFile("./retrained_graph.pb", 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    _ = tf.import_graph_def(graph_def, name='')
+
+with tf.Session() as sess:
+    # Feed the image_data as input to the graph and get first prediction
+    softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+    predictions = sess.run(softmax_tensor, \
+                 {'DecodeJpeg/contents:0': image_data})
+        
+    # Sort to show labels of first prediction in order of confidence
+    top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+    firstElt = top_k[0];
+
+    if (label_lines[firstElt] == "potato"):
+        print('Potato')
+    else:
+        print('Not Potato')
